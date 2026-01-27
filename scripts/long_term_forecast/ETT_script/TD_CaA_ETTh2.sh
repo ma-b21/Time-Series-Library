80f13976622835190d8eee3b245c98a9a7fd7d14
MAPU <mb_thu@163.com>
2026-01-27 23:10:09 +0800

    h2 finish

--- scripts/long_term_forecast/ETT_script/TD_CaA_ETTh2.sh
+++ scripts/long_term_forecast/ETT_script/TD_CaA_ETTh2.sh
@@ -1,23 +1,129 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 model_name=TD_CaA
 seq_len=96
-pred_lens=(96)
 
-for pred_len in "${pred_lens[@]}"; do
-    python run.py \
+# python run.py \
+#       --task_name long_term_forecast \
+#       --is_training 1 \
+#       --root_path ./dataset/ETT-small/ \
+#       --data_path ETTh2.csv \
+#       --model_id ETTh2_${seq_len}_96 \
+#       --model $model_name \
+#       --data ETTh2 \
+#       --features M \
+#       --seq_len $seq_len \
+#       --label_len 48 \
+#       --pred_len 96 \
+#       --e_layers 2 \
+#       --d_layers 1 \
+#       --factor 3 \
+#       --enc_in 7 \
+#       --dec_in 7 \
+#       --c_out 7 \
+#       --d_ff 128 \
+#       --des 'Exp' \
+#       --itr 1 \
+#       --d_model 64 \
+#       --dropout 0.2 \
+#       --batch_size 4 \
+#       --moving_avg 3 \
+#       --learning_rate 0.0025 \
+#       --k_lookback 96 \
+#       --train_epochs 20 \
+#       --fft \
+#       --patience 3 \
+#       --method "Dynamic"\
+#       --hidden 25 \
+#       --bias \
+#       --interact
+
+
+# python run.py \
+#       --task_name long_term_forecast \
+#       --is_training 1 \
+#       --root_path ./dataset/ETT-small/ \
+#       --data_path ETTh2.csv \
+#       --model_id ETTh2_${seq_len}_192 \
+#       --model $model_name \
+#       --data ETTh2 \
+#       --features M \
+#       --seq_len $seq_len \
+#       --label_len 48 \
+#       --pred_len 192 \
+#       --e_layers 2 \
+#       --d_layers 1 \
+#       --factor 3 \
+#       --enc_in 7 \
+#       --dec_in 7 \
+#       --c_out 7 \
+#       --d_ff 128 \
+#       --des 'Exp' \
+#       --itr 1 \
+#       --d_model 64 \
+#       --dropout 0.2 \
+#       --batch_size 32 \
+#       --moving_avg 3 \
+#       --learning_rate 0.02 \
+#       --k_lookback 96 \
+#       --train_epochs 20 \
+#       --fft \
+#       --patience 3 \
+#       --method "Dynamic"\
+#       --hidden 26 \
+#       --bias \
+#       --interact
+
+
+# python run.py \
+#       --task_name long_term_forecast \
+#       --is_training 1 \
+#       --root_path ./dataset/ETT-small/ \
+#       --data_path ETTh2.csv \
+#       --model_id ETTh2_${seq_len}_336 \
+#       --model $model_name \
+#       --data ETTh2 \
+#       --features M \
+#       --seq_len $seq_len \
+#       --label_len 48 \
+#       --pred_len 336 \
+#       --e_layers 2 \
+#       --d_layers 1 \
+#       --factor 3 \
+#       --enc_in 7 \
+#       --dec_in 7 \
+#       --c_out 7 \
+#       --d_ff 128 \
+#       --des 'Exp' \
+#       --itr 1 \
+#       --d_model 64 \
+#       --dropout 0.1 \
+#       --batch_size 32 \
+#       --moving_avg 3 \
+#       --learning_rate 0.0135 \
+#       --k_lookback 96 \
+#       --train_epochs 20 \
+#       --fft \
+#       --patience 3 \
+#       --method "Dynamic"\
+#       --hidden 26 \
+#       --bias \
+#       --interact
+
+
+python run.py \
       --task_name long_term_forecast \
       --is_training 1 \
       --root_path ./dataset/ETT-small/ \
       --data_path ETTh2.csv \
-      --model_id ETTh2_${seq_len}_${pred_len} \
+      --model_id ETTh2_${seq_len}_720 \
       --model $model_name \
       --data ETTh2 \
       --features M \
       --seq_len $seq_len \
       --label_len 48 \
-      --pred_len $pred_len \
+      --pred_len 720 \
       --e_layers 2 \
       --d_layers 1 \
       --factor 3 \
@@ -27,13 +133,16 @@ for pred_len in "${pred_lens[@]}"; do
       --d_ff 128 \
       --des 'Exp' \
       --itr 1 \
-      --d_model 128 \
-      --dropout 0.3 \
-      --batch_size 8 \
+      --d_model 64 \
+      --dropout 0.1 \
+      --batch_size 32 \
       --moving_avg 3 \
-      --learning_rate 0.00025 \
-      --k_lookback 8 \
+      --learning_rate 0.01375 \
+      --k_lookback 96 \
       --train_epochs 20 \
       --fft \
-      --patience 10
-done
+      --patience 3 \
+      --method "Dynamic"\
+      --hidden 26 \
+      --bias \
+      --interact
